3f09ae7a65715ac1a7ef085b8099b9fcfc8674c0
Manoj S <93467109+Manoj632004@users.noreply.github.com>
2025-03-16 22:56:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,23 @@
 from flask import Flask, render_template, request
-from model import translate, get_answer
+from model import translate, model  #Import functions from model.py
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    context = None
-    question = None
-    answer = None
-    language_token = None
+    return render_template('index.html')
 
-    if request.method == 'POST':
-        if 'contextInput' in request.form:
-            # First submission - Context input
-            context = request.form.get('contextInput')
-            language_token = request.form.get('languageToken')
-            if language_token == "1":
-                context = translate(context)
-        elif 'questionInput' in request.form:
-            # Second submission - Question input
-            context = request.form.get('context')
-            language_token = request.form.get('language_token')
-            question = request.form.get('questionInput')
-            answer = get_answer(question, context)
+@app.route('/result', methods=['POST'])
+def result():
+    content = request.form['content'] 
+    translated_text = translate(content) 
+    return render_template('main.html', content=content, translated_text=translated_text)
 
-    return render_template('home.html', context=context, question=question, answer=answer, language_token=language_token)
+@app.route('/answer', methods=['POST'])
+def answer():
+    question = request.form['question']  
+    answer_text = model(question)  
+    return render_template('main.html', content=request.form['content'], translated_text=request.form['translated_text'], answer=answer_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
