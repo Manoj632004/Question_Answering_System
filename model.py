cc145ecf1f020164d0774f321ddda05196558e20
Manoj S <93467109+Manoj632004@users.noreply.github.com>
2025-03-16 23:15:40 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,30 @@
-# model.py
-from transformers import pipeline
-from googletrans import Translator
+import torch
+from transformers import BertForQuestionAnswering, BertTokenizer
 
-translator = Translator()
-# Initialize the question-answering model
-oracle = pipeline(model="deepset/roberta-base-squad2")
+def model(context, question):
+    model = BertForQuestionAnswering.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+    tokenizer = BertTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+    
+    # Encode Input
+    encoding = tokenizer.encode_plus(text=question, text_pair=context, return_tensors="pt")
+    
+    inputs = encoding['input_ids']
+    sentence_embedding = encoding['token_type_ids']
+    
+    start_scores, end_scores = model(input_ids=inputs, token_type_ids=sentence_embedding)
+    
+    start_index = torch.argmax(start_scores)
+    end_index = torch.argmax(end_scores)
+    
+    tokens = tokenizer.convert_ids_to_tokens(inputs[0])
+    answer = ' '.join(tokens[start_index:end_index+1])
+    
+    corrected_answer = ''
+    for word in answer.split():
+        if word.startswith('##'):
+            corrected_answer += word[2:]
+        else:
+            corrected_answer += ' ' + word
+    
+    return corrected_answer.strip()
 
-# Dummy translation function (replace this with your actual code)
-def translate(tamil_text):
-    translated = str(translator.translate(tamil_text,dest  = 'en').text)
-    return translated
-
-# Function to get an answer based on the question and context
-def get_answer(question, context):
-    return oracle(question=question, context=context)['answer']
